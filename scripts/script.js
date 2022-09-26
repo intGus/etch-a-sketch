6ec9b9b45fb40f2ea4b2d71f30dbf6e630ef6d69
intGus <46197948+intGus@users.noreply.github.com>
2022-09-25 21:29:30 -0400

    Add main functionality to the app, plus basic CSS to clear the grid

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector('#container');
+const button = document.querySelector('#clear')
+
+container.style.length = '960px';
+container.style.width = '960px';
+container.style.display ='grid';
+
+//(`repeat(${rowtot}, 1fr)`);
+
+container.style.gridTemplateColumns = (`repeat(64, 1fr))`);
+
+let column = 1;
+let row = 1;
+
+for (let i = 1; i <= 4096; i++) {
+  const square = document.createElement('div');
+  //square.textContent = i;
+  square.style.border = '1px solid black';
+  square.style.aspectRatio = '1'
+  square.style.gridRow = row;
+  square.style.gridColumn = column;
+  column +=1;
+  if (column === 65){
+    row += 1;
+    column = 1;
+  }
+  container.appendChild(square);
+}
+
+container.addEventListener("mouseover", function (e) {
+  e.target.classList.add('hovered');
+  //e.target.style.background = 'blue'
+  //console.log(e.target)
+});
+
+let allDivs = container.querySelectorAll(":scope > div");
+
+button.addEventListener('click', () => {
+  console.log('clicked button')
+  allDivs.forEach((item) => item.classList.remove('hovered'))
+});
